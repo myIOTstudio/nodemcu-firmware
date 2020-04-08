84455a2076d57b3538fb7c78b5b18f62b1433ccd
Sunny <sunny.xiao@qq.com>
2020-04-08 10:10:45 +0800

    修改了默认加载模块

--- app/include/user_config.h
+++ app/include/user_config.h
@@ -108,7 +108,7 @@ extern void luaL_assertfail(const char *file, int line, const char *message);
  * if defined hostname is invalid: hostname will default to NODE-xxxxxx (xxxxxx being last 3 octets of MAC address)
 */
 //#define WIFI_STA_HOSTNAME "NodeMCU"
-//#define WIFI_STA_HOSTNAME_APPEND_MAC
+#define WIFI_STA_HOSTNAME_APPEND_MAC
 
 //#define WIFI_SMART_ENABLE
 
